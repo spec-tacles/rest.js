97f4db07dcf45634b260e89d61737e2eea3fab3d
Stilldabomb <stilldabomb@gmail.com>
2019-09-07 18:45:08 -0700

    feat: added prefix parameter to RedisMutex

--- src/mutexes/Redis.ts
+++ src/mutexes/Redis.ts
@@ -15,12 +15,12 @@ declare module 'ioredis' {
 
 export default class RedisMutex extends RatelimitMutex {
 	public static readonly keys = {
-		global: 'global',
-		remaining: (route: string) => `${route}:remaining`,
-		limit: (route: string) => `${route}:limit`,
+		global: (prefix?: string) => `${ prefix ? `${ prefix }:` : '' }global`,
+		remaining: (route: string, prefix?: string) => `${ prefix ? `${ prefix }:` : '' }${route}:remaining`,
+		limit: (route: string, prefix?: string) => `${ prefix ? `${ prefix }:` : '' }${route}:limit`,
 	};
 
-	constructor(public readonly redis: Redis) {
+	constructor(public readonly redis: Redis, public readonly prefix?: string) {
 		super();
 		redis.defineCommand('gettimeout', {
 			numberOfKeys: 3,
@@ -31,14 +31,14 @@ export default class RedisMutex extends RatelimitMutex {
 	public async set(route: string, limits: Partial<Ratelimit>): Promise<void> {
 		const pipe = this.redis.pipeline();
 		if (limits.timeout) {
-			if (limits.global) pipe.set(RedisMutex.keys.global, true, 'px', limits.timeout);
-			else pipe.pexpire(RedisMutex.keys.remaining(route), limits.timeout);
+			if (limits.global) pipe.set(RedisMutex.keys.global(this.prefix), true, 'px', limits.timeout);
+			else pipe.pexpire(RedisMutex.keys.remaining(route, this.prefix), limits.timeout);
 		}
-		if (limits.limit) pipe.set(RedisMutex.keys.limit(route), limits.limit);
+		if (limits.limit) pipe.set(RedisMutex.keys.limit(route, this.prefix), limits.limit);
 		await pipe.exec();
 	}
 
 	protected async getTimeout(route: string) {
-		return this.redis.gettimeout(RedisMutex.keys.remaining(route), RedisMutex.keys.limit(route), RedisMutex.keys.global, 1e3);
+		return this.redis.gettimeout(RedisMutex.keys.remaining(route, this.prefix), RedisMutex.keys.limit(route, this.prefix), RedisMutex.keys.global(this.prefix), 1e3);
 	}
 }
