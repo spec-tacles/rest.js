705737fb4419cb8ce78cc8498d65090d92f915ca
Stilldabomb <Stilldabomb@users.noreply.github.com>
2019-09-27 09:12:58 -0700

    fix: export TokenType enum (#4)
    * fix: export TokenType enum
    
    * fix: export TokenType enum

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import RedisMutex from './mutexes/Redis';
 import Bucket from './structures/Bucket';
-import Rest, { Options } from './structures/Rest';
+import Rest, { Options, TokenType } from './structures/Rest';
 import Events from './types/Events';
 import Request from './types/Request';
 import Retry, { RetryReason } from './types/Retry';
 
-export { RedisMutex, Bucket, Options, Rest, Events, Request, Retry, RetryReason };
+export { RedisMutex, Bucket, Options, TokenType, Rest, Events, Request, Retry, RetryReason };
 
 export default Rest;
