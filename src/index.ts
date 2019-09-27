036008661cf44d674520b7069f944d452a3602f8
Stilldabomb <stilldabomb@gmail.com>
2019-09-27 00:43:42 -0700

    fix: export TokenType enum

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import RedisMutex from './mutexes/Redis';
 import Bucket from './structures/Bucket';
-import Rest, { Options } from './structures/Rest';
+import Rest, { Options, TokenType } from './structures/Rest';
 import Events from './types/Events';
 import Request from './types/Request';
 import Retry, { RetryReason } from './types/Retry';
